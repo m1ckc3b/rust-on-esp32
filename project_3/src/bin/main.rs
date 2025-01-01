da82cd594431f51abeca9483cb9fa67c3edb1496
mickceb <cebulski.mi@gmail.com>
2025-01-01 19:21:22 +0100

    first implementation

--- project_3/src/bin/main.rs
+++ project_3/src/bin/main.rs
@@ -2,20 +2,38 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output};
+use esp_hal::mcpwm::operator::PwmPinConfig;
+use esp_hal::mcpwm::timer::PwmWorkingMode;
+use esp_hal::mcpwm::{McPwm, PeripheralClockConfig};
 use esp_hal::prelude::*;
-use log::info;
 
 #[entry]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    esp_println::logger::init_logger_from_env();
+    let led = Output::new(peripherals.GPIO4, Level::Low);
 
-    let delay = Delay::new();
+    // initialize peripheral
+    let clock_cfg = PeripheralClockConfig::with_frequency(32.MHz()).unwrap();
+    let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
 
-    loop {
-        
-    }
+    // connect operator0 to timer0
+    mcpwm.operator0.set_timer(&mcpwm.timer0);
+    // connect operator0 to pin
+    let mut pwm_pin = mcpwm
+        .operator0
+        .with_pin_a(led, PwmPinConfig::UP_ACTIVE_HIGH);
 
+    // start timer with timestamp values in the range of 0..=99 and a frequency
+    // of 20 kHz
+    let timer_clock_cfg = clock_cfg
+        .timer_clock_with_frequency(99, PwmWorkingMode::Increase, 5000.kHz())
+        .unwrap();
+    mcpwm.timer0.start(timer_clock_cfg);
+
+    // pin will be high 50% of the time
+    pwm_pin.set_timestamp(50);
+
+    loop {}
 }
