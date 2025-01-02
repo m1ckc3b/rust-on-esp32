ff3e73dc8f93ebd5f4605c7f7824fb4e553ac148
mickceb <cebulski.mi@gmail.com>
2025-01-02 12:51:43 +0100

    a try with 2 timer_clk for increasing and decreasing

--- project_3/src/bin/main.rs
+++ project_3/src/bin/main.rs
@@ -6,7 +6,7 @@ use esp_hal::gpio::{Level, Output};
 use esp_hal::mcpwm::operator::PwmPinConfig;
 use esp_hal::mcpwm::timer::PwmWorkingMode;
 use esp_hal::mcpwm::{McPwm, PeripheralClockConfig};
-use esp_hal::prelude::*;
+use esp_hal::{delay, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -20,6 +20,7 @@ fn main() -> ! {
 
     // connect operator0 to timer0
     mcpwm.operator0.set_timer(&mcpwm.timer0);
+
     // connect operator0 to pin
     let mut pwm_pin = mcpwm
         .operator0
@@ -27,17 +28,24 @@ fn main() -> ! {
 
     // start timer with timestamp values in the range of 0..=99 and a frequency
     // of 20 kHz
-    let timer_clock_cfg = clock_cfg
+    let timer_clock_cfg_increase = clock_cfg
         .timer_clock_with_frequency(99, PwmWorkingMode::Increase, 20.kHz())
         .unwrap();
-    mcpwm.timer0.start(timer_clock_cfg);
 
-    // pin will be high 50% of the time
-    pwm_pin.set_timestamp(50);
+    let timer_clock_cfg_decrease = clock_cfg
+    .timer_clock_with_frequency(99, PwmWorkingMode::Decrease, 20.kHz())
+    .unwrap();
 
+    mcpwm.timer0.start(timer_clock_cfg_increase);
+    mcpwm.timer1.start(timer_clock_cfg_decrease);
+
+    let delay = delay::Delay::new();
+    
     loop {
         // TODO: increase the LED brightness
-
-        // TODO: decrease the LED brightness
+        // pin will be high 50% of the time
+        pwm_pin.set_timestamp(50);
+        delay.delay_millis(500);
+        pwm_pin.set_timestamp(0);
     }
 }
