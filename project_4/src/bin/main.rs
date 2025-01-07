c05b4444f492b1807fbf342596c5273b5aaf90e0
mickceb <cebulski.mi@gmail.com>
2025-01-07 14:28:50 +0100

    implemented polling logic

--- project_4/src/bin/main.rs
+++ project_4/src/bin/main.rs
@@ -4,23 +4,31 @@
 use esp_backtrace as _;
 use esp_hal::delay::Delay;
 use esp_hal::prelude::*;
+use esp_hal::gpio::{Level, Output, Input, Pull};
+use esp_println::println;
 use log::info;
 
 #[entry]
 fn main() -> ! {
-    let _peripherals = esp_hal::init({
-        let mut config = esp_hal::Config::default();
-        config.cpu_clock = CpuClock::max();
-        config
-    });
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
     esp_println::logger::init_logger_from_env();
 
+    let mut buzzer = Output::new(peripherals.GPIO26, Level::Low);
+    let motion_sensor = Input::new(peripherals.GPIO27, Pull::Up);
+
     let delay = Delay::new();
+    info!("Detector is ready!");
+
     loop {
-        info!("Hello world!");
-        delay.delay(500.millis());
+
+        if motion_sensor.is_high() {
+            buzzer.set_high();
+            println!("Motion detected!Buzzer alarm!");
+            delay.delay_millis(500);
+            buzzer.set_low();
+            println!("Motion detected!Buzzer stop alarm!");
+        }
     }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
 }
