31d2b40b2abe1da32db83e3c47ab00a7be450143
mickceb <cebulski.mi@gmail.com>
2025-01-09 17:36:00 +0100

    update style

--- project_5/assets/script.js
+++ project_5/assets/script.js
@@ -1,6 +1,25 @@
-function preventAllEvents() {
-  document.addEventListener("click", (event) => event.preventDefault());
-  document.addEventListener("submit", (event) => event.preventDefault());
+let redIsOn = false
+let greenIsOn = false
+
+
+function toggleRed() {
+  fetch('/red').then(res => {
+    if (res.ok) {
+      redIsOn = !redIsOn
+      document.getElementById("redState").textContent = redIsOn ? "ON" : "OFF"
+    } else {
+      console.error("Erreur avec la requête")
+    }
+  })
 }
 
-preventAllEvents();
+function toggleGreen() {
+  fetch('/green').then(res => {
+    if (res.ok) {
+      greenIsOn = !greenIsOn
+      document.getElementById("greenState").textContent = greenIsOn ? "ON" : "OFF"
+    } else {
+      console.error("Erreur avec la requête")
+    }
+  })
+}
