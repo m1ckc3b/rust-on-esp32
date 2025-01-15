dc7d65bef36354e783856b80f4d965eb08aca4b7
mickceb <cebulski.mi@gmail.com>
2025-01-15 16:05:21 +0100

    using LEDC

--- project_3/src/bin/main.rs
+++ project_3/src/bin/main.rs
@@ -2,53 +2,58 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::gpio::{Level, Output};
-use esp_hal::mcpwm::operator::PwmPinConfig;
-use esp_hal::mcpwm::timer::PwmWorkingMode;
-use esp_hal::mcpwm::{McPwm, PeripheralClockConfig};
-use esp_hal::{delay, prelude::*};
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output},
+    ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed},
+    prelude::*,
+};
 
 #[entry]
 fn main() -> ! {
-    let resolution = 255;
-    let duration = 15;
     let peripherals = esp_hal::init(esp_hal::Config::default());
+    let delay = Delay::new();
 
     let led = Output::new(peripherals.GPIO4, Level::Low);
 
-    // initialize peripheral
-    let clock_cfg = PeripheralClockConfig::with_frequency(32.MHz()).unwrap();
-    let mut mcpwm = McPwm::new(peripherals.MCPWM0, clock_cfg);
-
-    // connect operator0 to timer0
-    mcpwm.operator0.set_timer(&mcpwm.timer0);
-
-    // connect operator0 to pin
-    let mut pwm_pin = mcpwm
-        .operator0
-        .with_pin_a(led, PwmPinConfig::UP_ACTIVE_HIGH);
-
-    // start timer with timestamp values in the range of 0..=255 and a frequency of 5 kHz
-    let timer_clock_cfg_increase = clock_cfg
-        .timer_clock_with_frequency(resolution, PwmWorkingMode::Increase, 5.kHz())
+    // LEDC
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    // Global Clock Source
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+    // Timer
+    let mut ledctimer = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    // Congig
+    ledctimer
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty12Bit, // PWM resolution (precision level of 4096)
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: 4u32.kHz(), // periode of 250 microseconds
+        })
+        .unwrap();
+    // channel
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: &ledctimer,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
         .unwrap();
 
-    mcpwm.timer0.start(timer_clock_cfg_increase);
+    let max_duty = 100u8;
+    let min_duty = 0_u8;
 
-    let delay = delay::Delay::new();
-    
     loop {
-        // TODO: increase/decrease the LED brightness
-        for i in 0..=resolution {
-            pwm_pin.set_timestamp(i);
-            delay.delay_millis(duration);
+        // Increasing brightness
+        for duty in min_duty..max_duty {
+            channel0.set_duty(duty).unwrap();
+            delay.delay_millis(10_u32);
+        }
 
-            if i == resolution {
-                for i in (0..=resolution).rev() {
-                    pwm_pin.set_timestamp(i);
-                    delay.delay_millis(duration);
-                }
-            }
+        // Decreasing
+        for duty in (min_duty..max_duty).rev() {
+            channel0.set_duty(duty).unwrap();
+            delay.delay_millis(10_u32);
         }
     }
 }
